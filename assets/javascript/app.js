2468d868070b50863b04b81b68b47b69c788cd51
Bailee Manzer <baileemanzer3@gmail.com>
2019-11-02 17:13:15 -0600

    el fin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,7 +31,7 @@ function count() {
 //if timer gets to 0, display final counts for correct, incorrect, and unanswered
 function finalScore() {
   $("#QUESTIONS").hide();
-  unanswered=10-(correct+incorrect);
+  unanswered=25-(correct+incorrect);
   var correctDiv=$("<div>");
   var incorrectDiv=$("<div>");
   var unansweredDiv=$("<div>");
@@ -158,4 +158,169 @@ $('.10').on('click', function(){
     incorrect++;
   }
   $('.10').hide();
+})
+//Question 11
+$('.11').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="jennifer") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.11').hide();
+})
+//Question 12
+$('.12').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="california") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.12').hide();
+})
+//Question 13
+$('.13').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="indiana") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.13').hide();
+})
+//Question 14
+$('.14').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="dig dug") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.14').hide();
+})
+//Question 15
+$('.15').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="mr. baldo") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.15').hide();
+})
+//Question 16
+$('.16').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="the department of energy") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.16').hide();
+})
+//Question 17
+$('.17').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="winona ryder") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.17').hide();
+})
+//Question 18
+$('.18').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="nancy") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.18').hide();
+})
+//Question 19
+$('.19').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="telekinesis") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.19').hide();
+})
+//Question 20
+$('.20').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="mirkwood") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.20').hide();
+})
+//Question 21
+$('.21').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="wrist rocket") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.21').hide();
+})
+//Question 22
+$('.22').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="three musketeers") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.22').hide();
+})
+//Question 23
+$('.23').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="the mind flayer") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.23').hide();
+})
+//Question 24
+$('.24').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="christmas lights") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.24').hide();
+})
+//Question 25
+$('.25').on('click', function(){
+  var answers=($(this).text());
+  if (answers==="4") {
+    correct++;
+  }
+  else {
+    incorrect++;
+  }
+  $('.25').hide();
 })
